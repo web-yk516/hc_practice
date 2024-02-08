db295b0498e90454dd89f8a5a21fcb2723e2e697
web-yk516 <outstandinglife718+happinesschan@gmail.com>
2024-02-08 11:38:40 +0900

    @juice_stockのみでジュースの種類と在庫数を管理するように修正

--- ruby/vending_machine/vending_machine.rb
+++ ruby/vending_machine/vending_machine.rb
@@ -7,50 +7,49 @@ require_relative './suica'
 class VendingMachine
   def initialize
     initialize_juice_stock
-    initialize_juice_quantity
     initialize_sales_amount
   end
 
   # 自動販売機の在庫
   def stock
-    @juice_quantity.map { |name, quantity| "#{name}: #{quantity}" }.join(', ')
+    @juice_stock.map { |juice_name, juice_info| "#{juice_name}: #{juice_info[:quantity]}" }.join(', ')
   end
 
   # 購入可能かどうか
   def purchase_available?(juice_name, suica)
-    juice = @juice_stock[juice_name]
-    quantity = @juice_quantity[juice_name]
+    juice_info = @juice_stock[juice_name]
 
     # チャージ残高が足りないもしくは在庫がない
-    return false if juice.nil? || quantity.nil?
+    return false if juice_info.nil?
 
-    suica.current_balance >= juice.price && quantity.positive?
+    suica.current_balance >= juice_info[:price] && juice_info[:quantity].positive?
   end
 
   # 購入処理
   def purchase(juice_name, suica)
-    juice = @juice_stock[juice_name]
+    juice_info = @juice_stock[juice_name]
 
     raise '指定されたジュースは購入できません' unless purchase_available?(juice_name, suica)
 
     # juiceの個数を減らす
-    @juice_quantity[juice_name] -= 1
+    juice_info[:quantity] -= 1
     # 売上を増やす
-    @sales_amount += juice.price
+    @sales_amount += juice_info[:price]
     # チャージ残高をjuice分減らす
-    suica.reduce_balance(juice.price)
+    suica.reduce_balance(juice_info[:price])
   end
 
   # 購入可能なドリンクのリスト
   def available_drinks
-    @juice_stock.select { |name| purchase_available?(name, Suica.new) }.keys.join(', ')
+    @juice_stock.select { |juice_name| purchase_available?(juice_name, Suica.new) }.keys.join(', ')
   end
 
   # 在庫を補充
   def refill(juice_name, quantity)
-    raise '指定されたジュースは存在しません' unless @juice_stock.key?(juice_name)
+    juice_info = @juice_stock[juice_name]
+    raise '指定されたジュースは存在しません' unless juice_info
 
-    @juice_quantity[juice_name] += quantity
+    juice_info[:quantity] += quantity
   end
 
   # 売上金額
@@ -61,16 +60,9 @@ class VendingMachine
   def initialize_juice_stock
     # juiceの在庫
     @juice_stock = {
-      'ペプシ' => Juice.new('ペプシ', 150),
-      'モンスター' => Juice.new('モンスター', 230),
-      'いろはす' => Juice.new('いろはす', 120)
-    }
-  end
-
-  def initialize_juice_quantity
-    # juiceの在庫数
-    @juice_quantity = {
-      'ペプシ' => 5, 'モンスター' => 5, 'いろはす' => 5
+      'ペプシ' => { price: 150, quantity: 5 },
+      'モンスター' => { price: 230, quantity: 5 },
+      'いろはす' => { price: 120, quantity: 5 }
     }
   end
 
