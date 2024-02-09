91b9a9ec6b1b5daa24301a83b8dbb35a80c15e2b
web-yk516 <outstandinglife718+happinesschan@gmail.com>
2024-02-09 14:34:51 +0900

    @juice_stockにJuiceインスタンスと在庫数も持ったハッシュを配列で持つように修正

--- ruby/vending_machine/vending_machine.rb
+++ ruby/vending_machine/vending_machine.rb
@@ -12,41 +12,41 @@ class VendingMachine
 
   # 自動販売機の在庫
   def stock
-    @juice_stock.map { |juice_name, juice_info| "#{juice_name}: #{juice_info[:quantity]}" }.join(', ')
+    @juice_stock.map { |juice_info| "#{juice_info[:juice].name}: #{juice_info[:quantity]}" }.join(', ')
   end
 
   # 購入可能かどうか
   def purchase_available?(juice_name, suica)
-    juice_info = @juice_stock[juice_name]
+    juice_info = find_juice_info_by_name(juice_name)
 
     # チャージ残高が足りないもしくは在庫がない
     return false if juice_info.nil?
 
-    suica.current_balance >= juice_info[:price] && juice_info[:quantity].positive?
+    suica.current_balance >= juice_info[:juice].price && juice_info[:quantity].positive?
   end
 
   # 購入処理
   def purchase(juice_name, suica)
-    juice_info = @juice_stock[juice_name]
+    juice_info = find_juice_info_by_name(juice_name)
 
     raise '指定されたジュースは購入できません' unless purchase_available?(juice_name, suica)
 
     # juiceの個数を減らす
     juice_info[:quantity] -= 1
     # 売上を増やす
-    @sales_amount += juice_info[:price]
+    @sales_amount += juice_info[:juice].price
     # チャージ残高をjuice分減らす
-    suica.reduce_balance(juice_info[:price])
+    suica.reduce_balance(juice_info[:juice].price)
   end
 
   # 購入可能なドリンクのリスト
-  def available_drinks
-    @juice_stock.select { |juice_name| purchase_available?(juice_name, Suica.new) }.keys.join(', ')
+  def available_drinks(suica)
+    @juice_stock.select { |info| purchase_available?(info[:juice].name, suica) }.map  { |info| info[:juice].name }.join(', ')
   end
 
   # 在庫を補充
   def refill(juice_name, quantity)
-    juice_info = @juice_stock[juice_name]
+    juice_info = find_juice_info_by_name(juice_name)
     raise '指定されたジュースは存在しません' unless juice_info
 
     juice_info[:quantity] += quantity
@@ -59,11 +59,11 @@ class VendingMachine
 
   def initialize_juice_stock
     # juiceの在庫
-    @juice_stock = {
-      'ペプシ' => { price: 150, quantity: 5 },
-      'モンスター' => { price: 230, quantity: 5 },
-      'いろはす' => { price: 120, quantity: 5 }
-    }
+    @juice_stock = [
+      { juice: Juice.new('ペプシ', 150), quantity: 5 },
+      { juice: Juice.new('モンスター', 230), quantity: 5 },
+      { juice: Juice.new('いろはす', 120), quantity: 5 }
+    ]
   end
 
   private
@@ -71,6 +71,10 @@ class VendingMachine
   def initialize_sales_amount
     # 売上
     @sales_amount = 0
+  end
+
+  def find_juice_info_by_name(juice_name)
+    @juice_stock.find { |info| info[:juice].name == juice_name }
   end
 
   attr_reader :sales_amount
@@ -93,7 +97,7 @@ end
 puts "現在のSuica残高: #{suica.current_balance}円"
 puts "現在のジュース在庫: #{vending_machine.stock}"
 puts "売上金額: #{vending_machine.revenue}円"
-puts "購入可能なドリンクのリスト: #{vending_machine.available_drinks}"
+puts "購入可能なドリンクのリスト: #{vending_machine.available_drinks(suica)}"
 
 vending_machine.refill('モンスター', 10)
 puts "現在のジュース在庫: #{vending_machine.stock}"
